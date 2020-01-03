06ad3f75c789d136cd7ea501e5db85431a539341
jeohyn <42138399+jeohyn@users.noreply.github.com>
2020-01-03 21:06:13 +0900

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>wallpaper</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div id="ico1"><a href="list.php?list=<?=1?>"><img src="1.png" class="icon"></a></div>
+      <div id="ico2"><a href="list.php?list=<?=2?>"><img src="2.png" class="icon"></a></div>
+      <div id="ico3"><a href="list.php?list=<?=3?>"><img src="3.png" class="icon"></a></div>
+      <div id="ico4"><a href="list.php?list=<?=4?>"><img src="4.png" class="icon"></a></div>
+    </div>
+  </body>
+</html>
